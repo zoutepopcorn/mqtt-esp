22505497abc75497c6f04daee5fb99ea372c044f
zoutepopcorn <zoutepopcorn@hotmail.com>
2019-05-30 15:48:52 +0200

    Create config.del.h

---
+++ arduino/config.del.h
@@ -0,0 +1,12 @@
+// WIFI config
+const char* ssid = "YOUR_WIFI";
+const char* password = "YOUR_PASS";
+
+// MQTT config
+const char* MQTT = "YOUR_MQTT_SERVER";
+const char* USER = "YOUR_MQTT_USER";
+const char* PASS = "YOUR_MQTT_PASS";
+const int PORT = YOUR_PORT_NUMBER_HTTP;
+
+// ESP config
+const int RATE == 115200;
